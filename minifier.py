2d8052dcf31246736f6ea74039145e8ba800318f
savioacp <savioacp@gmail.com>
2020-09-05 00:58:57 -0300

    Update minifier.py
    Made it actually behave like the passed options and some refac to make the code look better. Soon coding another minifier with more features...

--- minifier.py
+++ minifier.py
@@ -7,39 +7,46 @@ OUTPUT
   pure text
 """
 
+
 class Minifier:
 
   def __init__(self, content):
-    self.content = content
-    self.content=self.content.replace('$${','$${;')
+    self.content = content.replace('$${', '$${;')
 
   def minify(self, remove_comments=True, inject_collons=True, remove_tabs_and_break_lines=True):
-    content = self.remove_comments(content=self.content)
-    content = self.inject_collons(content=content)
-    content = self.remove_tabs_and_break_lines(content=content)
-    return content
+    if(remove_comments):
+      self.remove_comments()
 
-  #https://codereview.stackexchange.com/questions/148305/remove-comments-from-c-like-source-code
-  def remove_comments(self, content):
+    if(inject_collons):
+      self.inject_collons()
+
+    if(remove_tabs_and_break_lines):
+      self.remove_tabs_and_break_lines()
+      
+    return self.content
+
+  # https://codereview.stackexchange.com/questions/148305/remove-comments-from-c-like-source-code
+  def remove_comments(self):
     COMMENTS = re.compile(r'''
       ((?<!https:)(?<!http:)//[^\n]*(?:\n|$))    # Everything between // and the end of the line/file
       |                     # or
       (/\*.*?\*/)           # Everything between /* and */
     ''', re.VERBOSE)
-    return COMMENTS.sub('\n', content)
+    self.content = COMMENTS.sub('\n', self.content)
 
-  def inject_collons(self, content):
-    copy = content
-    buffer=''
-    for line in content.splitlines():
+  def inject_collons(self):
+    self.content
+    buffer = ''
+    for line in self.content.splitlines():
       p = re.compile(r".*?;[^A-Za-z0-9]*$", re.MULTILINE)
       q = re.compile(r".*(\$\$\{|\}\$\$)[^\w]*$", re.MULTILINE)
       if p.match(line) or q.match(line):
-        buffer+=line
+        buffer += line
       else:
-        if re.match('.*[A-Za-z0-9].*',line) and re.match('.*[^${}>][^A-Za-z0-9]*$', line):
-          buffer+="{};".format(line)
-    return buffer
+        if re.match('.*[A-Za-z0-9].*', line) and re.match('.*[^${}>][^A-Za-z0-9]*$', line):
+          buffer += "{};".format(line)
+      self.content = buffer
 
-  def remove_tabs_and_break_lines(self, content):
-    return re.sub(r"(?<=;)(\s+)(?=[^\s])", "", content).strip()
+  def remove_tabs_and_break_lines(self):
+    self.content = re.sub(
+        r"(?<=;)(\s+)(?=[^\s])", "", self.content).strip()
